6b3d31fe677e39c93106d0f44a7a3d4a592b5e33
Loh Boon How <90071640+Kelvin-Data@users.noreply.github.com>
2026-04-06 11:44:25 +0800

    Modify message table to use autoincrement for id

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('flask_ckeditor/message.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE message (
-    id integer primary key,
+c.execute("""CREATE TABLE message(
+    id integer primary key autoincrement,
     name text,
     email text,
     subscribe boolean,
